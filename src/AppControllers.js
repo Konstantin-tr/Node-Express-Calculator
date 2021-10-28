d190e99d31dd9e08cd3c9fa33e08d18495b2599c
Konstantin Siegl <konstantinsiegl2503@gmail.com>
2021-10-28 11:51:01 +0200

    Minus added

--- src/AppControllers.js
+++ src/AppControllers.js
@@ -3,6 +3,7 @@ var maths = require("./MathsModule");
 exports.mapControllers = function (app) {
   mapIndex(app);
   mapAdd(app);
+  mapMinus(app);
 };
 
 function mapAdd(app) {
@@ -11,6 +12,17 @@ function mapAdd(app) {
     var value2 = parseInt(req.params.val2);
 
     var result = maths.plus(value1, value2);
+
+    res.send("" + result);
+  });
+}
+
+function mapMinus(app) {
+  app.get("/calc/:val1/minus/:val2", (req, res) => {
+    var value1 = parseInt(req.params.val1);
+    var value2 = parseInt(req.params.val2);
+
+    var result = maths.minus(value1, value2);
 
     res.send("" + result);
   });
